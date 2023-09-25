9e1c60c2cbcb5f18391af7e8bd33341a1c28aae0
yurikohun <72153449+yurikohun@users.noreply.github.com>
2023-09-26 00:55:14 +0200

    Update AutoCS.py

--- AutoCS.py
+++ AutoCS.py
@@ -1,35 +1,34 @@
 import pyautogui
-import python_imagesearch
-from python_imagesearch.imagesearch import (imagesearch)
-from PIL import Image
 import time
+import win32api
+from python_imagesearch.imagesearch import imagesearch
 
 while True:
-    print("Válassz felbontást.")
+    print("Choose resolution.")
     print("1.: 1920x1080")
     print("2.: 1280x1024")
-    print("3.: Bezárás")
+    print("3.: Exit")
     x = int(input())
     if x > 3:
-        print("Ez nem egy lehetőség.")
+        print("This isn't an option.")
         time.sleep(3)
     elif x < 1:
-        print("Ez nem egy lehetőség.")
+        print("This isn't an option.")
         time.sleep(3)
     elif x == 1:
         while True:
-            pos = imagesearch("fullhd169.png")
+            pos = imagesearch("accept19201080.png")
             if pos[0] != -1:
-                print("position : ", pos[0], pos[1])
-                pyautogui.moveTo(pos[0], pos[1])
+                x, y = pyautogui.locateCenterOnScreen("accept19201080.png")
+                win32api.SetCursorPos((x, y))
                 pyautogui.leftClick()
                 break
     elif x == 2:
         while True:
-            pos = imagesearch("1280x1024.png")
+            pos = imagesearch("accept19201080.png")
             if pos[0] != -1:
-                print("position : ", pos[0], pos[1])
-                pyautogui.moveTo(pos[0], pos[1])
+                x, y = pyautogui.locateCenterOnScreen("accept19201080.png")
+                win32api.SetCursorPos((x, y))
                 pyautogui.leftClick()
                 break
     elif x == 3:
